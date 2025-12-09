cb38ddf613dfb6e22879ca76ab0c419002c5af67
rikkarth <ricardo.mendes@rikkarth.com>
2025-12-09 23:27:07 +0000

    add vimbegood, harpoon and keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Half-page down and keep cursor vertically centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- Half-page up and keep cursor vertically centered
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
